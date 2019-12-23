80c8d31dae64464908acfd5ee50fb0a1d9f6e110
Tomy <hongren24@163.com>
2019-12-23 21:20:16 +0800

    1

---
+++ flyrider-system/src/main/java/me/hongren/AppRun.java
@@ -0,0 +1,22 @@
+package me.hongren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAsync
+@SpringBootApplication
+@EnableTransactionManagement
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+
+}
