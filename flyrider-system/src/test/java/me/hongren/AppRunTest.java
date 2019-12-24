541888e39ad60c1d067cc65915677a7bd3f8a0f7
hongr <hongren24@163.com>
2019-12-24 18:03:43 +0800

    flyrider

--- flyrider-system/src/test/java/me/hongren/AppRunTest.java
+++ flyrider-system/src/test/java/me/hongren/AppRunTest.java
@@ -3,18 +3,22 @@ package me.hongren;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppRunTest
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppRunTest {
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
     }
 }
